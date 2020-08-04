8803975e2d33c4ff483cdb4a376bd85e3f1b30f7
Chris O'Malley <Chris.O'Malley@concepts.co.nz>
2020-08-04 15:56:52 +1200

    update runbook uri

--- pcs_automation/main.tf
+++ pcs_automation/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_automation_runbook" "untagged-resources" {
   runbook_type            = "PowerShell"
 
   publish_content_link {
-    uri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/c4935ffb69246a6058eb24f54640f53f69d3ac9f/101-automation-runbook-getvms/Runbooks/Get-AzureVMTutorial.ps1"
+    uri = "https://raw.githubusercontent.com/thecomalley/terraform-azure/master/pcs_automation/runbooks/Az-delete-untagged-resources.ps1"
   }
 }
 
